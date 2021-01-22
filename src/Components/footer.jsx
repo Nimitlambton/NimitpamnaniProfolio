7427df12022eab9fc3b4c1a17ca00257a75bc70c
Nimitpamnani <C0764944@mylambton.ca>
2021-01-22 18:26:43 +0530

    :D

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 class Footer extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
   }
 
   render() {
     return (
-      <footer className="fixed-bottom bg-dark text-center text-lg-start">
+      <footer className="fixed-bottom bg- text-center text-lg-start">
         <div className="text-center p-3">
           © 2020 Copyright: Nimit pamnani || Made with &hearts; in Toronto
           <span role="img" aria-label="flagCanada">
--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -10,14 +10,14 @@ class nav extends React.Component {
   render() {
     return (
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Nimitpamnani</Navbar.Brand>
+        <Navbar.Brand href="#home">Nimit Pamnani</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className=" ml-auto">
             <Nav.Link href="#link">Projects</Nav.Link>
-            <Nav.Link href="#link">Designs</Nav.Link>
-            <Nav.Link href="#link">Blogs</Nav.Link>
-            <Nav.Link href="#link">contact</Nav.Link>
+            <Nav.Link href="#link1">Designs</Nav.Link>
+            <Nav.Link href="#link2">Blogs</Nav.Link>
+            <Nav.Link href="#link3">contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
