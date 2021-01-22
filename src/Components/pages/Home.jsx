784b97104889aee24a55459c1b5609bb2e4c0cfb
Nimitpamnani <C0764944@mylambton.ca>
2021-01-23 00:53:10 +0530

    finalHome

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -13,7 +13,7 @@ class Home extends Component {
       <>
         <div>
           <Row>
-            <Col xs={6} md={4}>
+            <Col xs={6} md={6}>
               <Image
                 src="https://scontent.fdel20-1.fna.fbcdn.net/v/t31.0-8/11063426_903087189734684_4639040271096422440_o.jpg?_nc_cat=110&ccb=2&_nc_sid=174925&_nc_ohc=0vf2ZomJx4wAX8m7dix&_nc_ht=scontent.fdel20-1.fna&oh=b91c9d907ed784ed384ffaf5a08fa265&oe=602EA202"
                 roundedCircle
@@ -21,11 +21,11 @@ class Home extends Component {
                 width="400"></Image>
             </Col>
             <Col>
-              <div>
+              <div className="xs={6} md={6}">
                 <Jumbotron>
                   <h1>Hi 👋 , Thanks for Stoping by </h1>
                   <p>
-                    I am My name is Nimit pamnani ✨
+                    I am is Nimit pamnani ✨
                     <p>Mobile & web developer 💻 || Amateur Photographer 📸 </p>
                   </p>
                 </Jumbotron>
