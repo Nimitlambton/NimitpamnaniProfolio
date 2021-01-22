f0e8f071cb6c7eb447756044e72fba938b8c0c11
Nimitpamnani <C0764944@mylambton.ca>
2021-01-23 01:07:13 +0530

    QuickFix

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -12,21 +12,21 @@ class Home extends Component {
     return (
       <>
         <div>
-          <Row>
-            <Col xs={6} md={6}>
+          <Row className="justify-content-md-center">
+            <Col md="auto">
               <Image
                 src="https://scontent.fdel20-1.fna.fbcdn.net/v/t31.0-8/11063426_903087189734684_4639040271096422440_o.jpg?_nc_cat=110&ccb=2&_nc_sid=174925&_nc_ohc=0vf2ZomJx4wAX8m7dix&_nc_ht=scontent.fdel20-1.fna&oh=b91c9d907ed784ed384ffaf5a08fa265&oe=602EA202"
                 roundedCircle
                 height="400 "
                 width="400"></Image>
             </Col>
-            <Col>
+            <Col className="m2">
               <div className="xs={6} md={6}">
                 <Jumbotron>
                   <h1>Hi 👋 , Thanks for Stoping by </h1>
                   <p>
-                    I am Nimit pamnani ✨
-                    <p>Mobile & web developer 💻 || Amateur Photographer 📸 </p>
+                    I am Nimit pamnani ✨ Mobile & web developer 💻 || Amateur
+                    Photographer 📸
                   </p>
                 </Jumbotron>
               </div>
