4ad8c638bc8669615368a11e64a6ad7fb533490f
Nimitpamnani <C0764944@mylambton.ca>
2021-01-22 22:54:42 +0530

    Herojumbotron

--- src/Components/pages/projects.jsx
+++ src/Components/pages/projects.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
-
+import { ReactComponent as YourSvg } from "./freelance.svg";
 class Projects extends Component {
   constructor(props) {
     super();
   }
 
   render() {
-    return <h1> This is project </h1>;
+    return (
+      <>
+        <h1> This is project </h1>
+
+        <YourSvg></YourSvg>
+      </>
+    );
   }
 }
 
--- src/welcome.jsx
+++ src/welcome.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import Nav from "./Components/nav";
 import Footer from "./Components/footer";
 import Stage from "./Components/stage";
