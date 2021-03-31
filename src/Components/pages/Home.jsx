7bf3f0f10dfdd9477d63f42cbf6102d2b9f52e42
Nimitpamnani <C0764944@mylambton.ca>
2021-03-31 15:42:30 +0530

    withD

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -8,7 +8,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Resume from "../Assets/Resume.pdf";
 import HeroImg from "../Assets/heroImg.jpg";
 
-//dummy
 class Home extends Component {
   constructor(props) {
     super(props);
