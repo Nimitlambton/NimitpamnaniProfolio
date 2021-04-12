6e864f1152344479f9d96e32f296587c148a23c6
Nimitpamnani <C0764944@mylambton.ca>
2021-04-12 12:12:00 -0400

    message

--- src/Components/pages/Home.jsx
+++ src/Components/pages/Home.jsx
@@ -8,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Resume from "../Assets/Resume.pdf";
 import HeroImg from "../Assets/heroImg.jpg";
 
-//dummy
+//dumm
 class Home extends Component {
   constructor(props) {
     super(props);
